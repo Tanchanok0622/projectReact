3211d5cc1dde24fbf9aa3275f908f097aeb8a501
Tanchanok Ngamjaturong <94946248+Tanchanok0622@users.noreply.github.com>
2022-02-02 13:20:05 +0700

    Navigation Practice
    Navigation Practice

---
+++ 02-02-2022_2/components/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textTopStyle: {
+    fontSize: 25,
+    textAlign: "center",
+    marginBottom: 16,
+  },
+  textBottomStyle: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "grey",
+  },
+
+  sideMenuProfileIcon: {
+    resizeMode: "center",
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignSelf: "center",
+  },
+  iconStyle: {
+    width: 15,
+    height: 15,
+    marginHorizontal: 5,
+  },
+  customItem: {
+    padding: 16,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
+
+export { styles };
